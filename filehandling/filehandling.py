3ee354b25f8260dea6ae016bdfed077c7f5cc2d8
Sunny-2610 <sunnysinha9884@gmail.com>
2026-06-06 21:06:33 +0530

    learned about the file system

--- filehandling/filehandling.py
+++ filehandling/filehandling.py
@@ -1,2 +1,5 @@
-with open("filehandling/notes.txt", "w") as file:
-    file.write("Hello world")
+#with open("notes.txt", "w") as file:
+ #   file.write("Hello world")
+
+with open("notes.txt", "r") as file:
+    print(file.read())
