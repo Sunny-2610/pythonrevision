92c08dcc7b25c0183e466e3d17b2cbf96ec99486
Sunny-2610 <sunnysinha9884@gmail.com>
2026-06-06 12:34:05 +0530

    varibale projects completed

---
+++ varproj.py
@@ -0,0 +1,8 @@
+name = input("Enter your name :")
+age = int(input("Enter your age :"))
+college = input("Enter your college: ")
+
+print("\n---Profile---")
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
